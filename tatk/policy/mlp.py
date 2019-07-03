516e52a49638ac9b5b2f52181d2f8cab4bc03a2a
truthless11 <truthless11@gmail.com>
2019-07-03 15:11:36 +0800

    policy interface

--- tatk/policy/mlp.py
+++ tatk/policy/mlp.py
@@ -2,14 +2,38 @@
 import torch
 import numpy as np
 import logging
+import os
 from tatk.policy.policy import Policy
+from tatk.policy.rlmodule import MultiDiscretePolicy
+from tatk.policy.multiwoz.vector_multiwoz import MultiWozVector
+from tatk.policy.camrest.vector_camrest import CamrestVector
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class MLP(Policy):
     
-    def __init__(self, is_train=False):
+    def __init__(self, cfg, is_train=False, dataset='multiwoz'):
         self.is_train = is_train
+        self.policy = MultiDiscretePolicy(cfg).to(device=DEVICE)
+        
+        if dataset == 'multiwoz':
+            voc_file = os.path.join(
+                 os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                 'data/multiwoz/sys_da_voc.txt')
+            voc_opp_file = os.path.join(
+                 os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                 'data/multiwoz/usr_da_voc.txt')
+            self.vector = MultiWozVector(voc_file, voc_opp_file)
+        elif dataset == 'camrest':
+            voc_file = os.path.join(
+                 os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                 'data/camrest/sys_da_voc.txt')
+            voc_opp_file = os.path.join(
+                 os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                 'data/camrest/usr_da_voc.txt')
+            self.vector = CamrestVector(voc_file, voc_opp_file)
+        else:
+            raise NotImplementedError('unknown dataset {}'.format(dataset))
         
     def predict(self, state, sess=None):
         """
--- tatk/policy/rule.py
+++ tatk/policy/rule.py
@@ -2,11 +2,14 @@
 import torch
 import numpy as np
 import logging
+import os
 from tatk.policy.policy import Policy
 from tatk.policy.multiwoz.rule_based_multiwoz_bot import RuleBasedMultiwozBot
 from tatk.policy.multiwoz.policy_agenda_multiwoz import UserPolicyAgendaMultiWoz
+from tatk.policy.multiwoz.vector_multiwoz import MultiWozVector
 from tatk.policy.camrest.rule_based_camrest_bot import RuleBasedCamrestBot
 from tatk.policy.camrest.policy_agenda_camrest import UserPolicyAgendaCamrest
+from tatk.policy.camrest.vector_camrest import CamrestVector
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -15,21 +18,37 @@ class Rule(Policy):
     def __init__(self, is_train=False, dataset='multiwoz', character='sys'):
         self.is_train = is_train
         if dataset == 'multiwoz':
+            voc_file = os.path.join(
+                 os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                 'data/multiwoz/sys_da_voc.txt')
+            voc_opp_file = os.path.join(
+                 os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                 'data/multiwoz/usr_da_voc.txt')
             if character == 'sys':
+                self.vector = MultiWozVector(voc_file, voc_opp_file)
                 self.policy = RuleBasedMultiwozBot()
             elif character == 'usr':
+                self.vector = MultiWozVector(voc_opp_file, voc_file)
                 self.policy = UserPolicyAgendaMultiWoz()
             else:
                 raise NotImplementedError('unknown character {}'.format(character))
         elif dataset == 'camrest':
+            voc_file = os.path.join(
+                 os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                 'data/camrest/sys_da_voc.txt')
+            voc_opp_file = os.path.join(
+                 os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                 'data/camrest/usr_da_voc.txt')
             if character == 'sys':
+                self.vector = CamrestVector(voc_file, voc_opp_file)
                 self.policy = RuleBasedCamrestBot()
             elif character == 'usr':
+                self.vector = CamrestVector(voc_opp_file, voc_file)
                 self.policy = UserPolicyAgendaCamrest()
             else:
                 raise NotImplementedError('unknown character {}'.format(character))
         else:
-            raise NotImplementedError('unknown dataset {}'.format(character))
+            raise NotImplementedError('unknown dataset {}'.format(dataset))
         
     def predict(self, state, sess=None):
         """
@@ -40,7 +59,7 @@ class Rule(Policy):
             action : System act, with the form of (act_type, {slot_name_1: value_1, slot_name_2, value_2, ...})
         """
         s_vec = torch.Tensor(self.vector.state_vectorize(state))
-        a = self.policy.select_action(s_vec.to(device=DEVICE)).cpu()
+        a = self.policy.predict(s_vec.to(device=DEVICE)).cpu()
         action = self.vector.action_devectorize(a.numpy())
         
         return action
