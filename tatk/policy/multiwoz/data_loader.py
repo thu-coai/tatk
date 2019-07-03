139432b55519912393efe2981d4028c30a1d63b7
truthless11 <truthless11@gmail.com>
2019-07-03 16:30:18 +0800

    upload data

---
+++ tatk/policy/multiwoz/data_loader.py
@@ -0,0 +1,8 @@
+import os
+import zipfile
+
+class PolicyDataLoaderMultiWoz():
+    
+    def __init__():
+        root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+        pass
