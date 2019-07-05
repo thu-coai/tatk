e117b85e6f128dda36e635ebc4b9336ce0393e84
truthless11 <truthless11@gmail.com>
2019-07-05 09:36:53 +0800

    revise agenda

--- tatk/policy/multiwoz/policy_agenda_multiwoz.py
+++ tatk/policy/multiwoz/policy_agenda_multiwoz.py
@@ -77,7 +77,7 @@ class UserPolicyAgendaMultiWoz(Policy):
         self.domain_goals = self.goal.domain_goals
         self.agenda = Agenda(self.goal)
 
-    def predict(self, state, sys_action):
+    def predict(self, state):
         """
         Predict an user act based on state and preorder system action.
         Args:
@@ -89,6 +89,7 @@ class UserPolicyAgendaMultiWoz(Policy):
             reward (float): Reward given by user.
         """
         self.__turn += 2
+        sys_action = state['system_action']
 
         # At the beginning of a dialog when there is no NLU.
         if sys_action == "null":
