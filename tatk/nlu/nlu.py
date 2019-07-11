23f787871b7e1861291b871625a82804da8a38c8
newRuntimeException <43716234+newRuntimeException@users.noreply.github.com>
2019-07-11 18:15:08 +0800

    modify docs (#49)
    * add some docs for tatk.dst.rule
    
    * fix docs
    
    * modify docs in dialog_agent

--- tatk/nlu/nlu.py
+++ tatk/nlu/nlu.py
@@ -1,13 +1,13 @@
 """Natural language understanding interface."""
 from abc import ABCMeta, abstractmethod
 
+
 class NLU(metaclass=ABCMeta):
     """NLU module interface."""
 
     @abstractmethod
     def predict(self, utterance):
-        """
-        Predict the dialog act of a natural language utterance.
+        """Predict the dialog act of a natural language utterance.
         
         Args:
             utterance (str):
--- tatk/policy/vec.py
+++ tatk/policy/vec.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
 """Vector Interface"""
 
+
 class Vector():
-    
+
     def __init__(self):
         pass
-    
+
     def generate_dict(self):
-        """
-        init the dict for mapping state/action into vector
-        """
-    
+        """init the dict for mapping state/action into vector"""
+
     def state_vectorize(self, state):
-        """
-        vectorize a state
+        """vectorize a state
 
         Args:
-            state (tuple): Dialog state
+            state (tuple):
+                Dialog state
         Returns:
-            state_vec (np.array): Dialog state vector
+            state_vec (np.array):
+                Dialog state vector
         """
         raise NotImplementedError
-        
+
     def action_devectorize(self, action_vec):
-        """
-        recover an action
+        """recover an action
         
         Args:
-            action_vec (np.array): Dialog act vector
+            action_vec (np.array):
+                Dialog act vector
         Returns:
-            action (tuple): Dialog act
+            action (tuple):
+                Dialog act
         """
         raise NotImplementedError
-    
