44a486d77e6d8c052021a16cdeda43c81d75f11e
newRuntimeException <wdz15@mails.tsinghua.edu.cn>
2019-07-11 20:15:11 +0800

    modify docs

--- tatk/policy/vhus/multiwoz/vhus.py
+++ tatk/policy/vhus/multiwoz/vhus.py
@@ -10,9 +10,10 @@ from tatk.policy.vhus.multiwoz.usermanager import UserDataManager
 from tatk.policy.vhus.usermodule import VHUS
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
+
+
 class UserPolicyVHUS(Policy):
-    
+
     def __init__(self):
         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json'), 'r') as f:
             config = json.load(f)
@@ -22,25 +23,28 @@ class UserPolicyVHUS(Policy):
         self.goal_gen = GoalGenerator()
         self.manager = manager
         self.user.eval()
-        
+
         self.load(config['load'])
-        
+
     def init_session(self):
         self.time_step = -1
         self.topic = 'NONE'
         self.goal = self.goal_gen.get_user_goal()
         self.goal_input = torch.LongTensor(self.manager.get_goal_id(self.manager.usrgoal2seq(self.goal)))
         self.goal_len_input = torch.LongTensor([len(self.goal_input)]).squeeze()
-        self.sys_da_id_stack = [] # to save sys da history
+        self.sys_da_id_stack = []  # to save sys da history
 
     def predict(self, state):
-        """
-        Predict an user act based on state and preorder system action.
+        """Predict an user act based on state and preorder system action.
+
         Args:
-            state (tuple): Dialog state.
+            state (tuple):
+                Dialog state.
         Returns:
-            usr_action (tuple): User act.
-            session_over (boolean): True to terminate session, otherwise session continues.
+            usr_action (tuple):
+                User act.
+            session_over (boolean):
+                True to terminate session, otherwise session continues.
         """
         sys_action = state['system_action']
         sys_seq_turn = self.manager.sysda2seq(self.manager.ref_data2stand(sys_action), self.goal)
@@ -50,9 +54,9 @@ class UserPolicyVHUS(Policy):
         sys_seq = torch.LongTensor(padding(self.sys_da_id_stack, max_sen_len))
         usr_a, terminal = self.user.select_action(self.goal_input, self.goal_len_input, sys_seq, sys_seq_len)
         usr_action = self.manager.usrseq2da(self.manager.id2sentence(usr_a), self.goal)
-        
+
         return capital(usr_action), terminal
-    
+
     def load(self, filename):
         user_mdl = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename + '_simulator.mdl')
         if os.path.exists(user_mdl):
