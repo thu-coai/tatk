988d5773a4a2a062cb26703df96156fc49b5d4e3
newRuntimeException <43716234+newRuntimeException@users.noreply.github.com>
2019-07-12 20:10:58 +0800

    modify docs (#61)

--- tatk/dst/rule/multiwoz/dst_util.py
+++ tatk/dst/rule/multiwoz/dst_util.py
@@ -36,8 +36,8 @@ def minDistance(word1, word2):
 
 
 def normalize_value(value_set, domain, slot, value):
-    """
-    Normalized the value produced by NLU module to map it to the ontology value space.
+    """Normalized the value produced by NLU module to map it to the ontology value space.
+
     Args:
         value_set (dict):
             The value set of task ontology.
@@ -169,6 +169,7 @@ def _match_duration(value):
     if mat is not None and len(mat.groups()) > 0:
         return mat.groups()[0]
     return None
+
 
 if __name__ == "__main__":
     # value_set = json.load(open('../../../data/multiwoz/db/db_values.json'))
