0f42ed08048c6674e25affa8bfe46d13f3019c2d
zhangzthu <zhangz.goal@gmail.com>
2019-07-31 21:02:39 +0800

    add deal or not auto download (#82)
    * add deal_or_not model
    
    * move init_state to tatk/util/multiwoz
    
    * add dealornot test scripts
    
    * a) upload deal_or_not pretrained models; b) add deal_or_not README
    
    * small changes
    
    * a) MDBT relative path, README; b) deal_or_not tutorial; c) license issue
    
    * add mdbt readme
    
    * add tutorial of deal_or_not dataset in /tutorials
    
    * fix mdbt path bug
    
    * fix mdbt path bugs
    
    * upload mdbt auto download
    
    * add dealornot auto download

--- tatk/e2e/rnn_rollout/deal_or_not/model.py
+++ tatk/e2e/rnn_rollout/deal_or_not/model.py
@@ -7,22 +7,21 @@ from tatk import get_root_path
 import os
 import zipfile
 from tatk.util.file_util import cached_path
+import shutil
 
 class DealornotAgent(RNNRolloutAgent):
     """The Rnn Rollout model for DealorNot dataset."""
     def __init__(self, name, args, sel_args, train=False, diverse=False, max_total_len=100,
                  model_url='https://tatk-data.s3-ap-northeast-1.amazonaws.com/rnnrollout_dealornot.zip'):
-        self.config_path = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'configs')
+        self.config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'configs')
 
         if not os.path.exists(self.config_path):
             os.mkdir(self.config_path)
         _model_path = os.path.join(self.config_path, 'models')
+        self.model_path = _model_path
         if not os.path.exists(_model_path):
-            os.mkdir(_model_path)
-        if not os.path.exists(os.path.join(_model_path, 'rnn_model_state_dict.th')):
-            # download and unzip pretrained models
-            dir = cached_path(model_url, _model_path)
-            print(dir)
+            os.makedirs(_model_path)
+
         self.data_path = os.path.join(get_root_path(), args.data)
         domain = get_domain(args.domain)
         corpus = RnnModel.corpus_ty(domain, self.data_path, freq_cutoff=args.unk_threshold, verbose=True,
@@ -41,5 +40,54 @@ class DealornotAgent(RNNRolloutAgent):
         super(DealornotAgent, self).__init__(model, sel_model, args, name, train, diverse, max_total_len)
         self.vis = args.visual
 
+        self.file_url = model_url
+
+        self.auto_download()
+
+    def auto_download(self):
+        """Automatically download the pretrained model and necessary data."""
+        if os.path.exists(os.path.join(self.config_path, 'model/rnn_model_state_dict.th')) and \
+            os.path.exists(os.path.join(self.config_path, 'selection_model_state_dict.th')):
+            return
+        models_dir = os.path.join(self.config_path, 'models')
+        cached_path(self.file_url, models_dir)
+        files = os.listdir(models_dir)
+        target_file = ''
+        for name in files:
+            if name.endswith('.json'):
+                target_file = name[:-5]
+        try:
+            assert target_file in files
+        except Exception as e:
+            print('allennlp download file error: RnnRollout Deal_or_Not data download failed.')
+            raise e
+        zip_file_path = os.path.join(models_dir, target_file + '.zip')
+        shutil.copyfile(os.path.join(models_dir, target_file), zip_file_path)
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            zip_ref.extractall(models_dir)
+
 def get_context_generator(context_file):
     return utils.ContextGenerator(os.path.join(get_root_path(), context_file))
+
+# if __name__ == '__main__':
+#     config_path = './configs'
+#     if os.path.exists(os.path.join(config_path, 'model/rnn_model_state_dict.th')) and \
+#             os.path.exists(os.path.join(config_path, 'selection_model_state_dict.th')):
+#         exit()
+#     models_dir = os.path.join(config_path, 'models')
+#     file_url = 'https://tatk-data.s3-ap-northeast-1.amazonaws.com/rnnrollout_dealornot.zip'
+#     cached_path(file_url, models_dir)
+#     files = os.listdir(models_dir)
+#     target_file = ''
+#     for name in files:
+#         if name.endswith('.json'):
+#             target_file = name[:-5]
+#     try:
+#         assert target_file in files
+#     except Exception as e:
+#         print('allennlp download file error: RnnRollout Deal_or_Not data download failed.')
+#         raise e
+#     zip_file_path = os.path.join(models_dir, target_file + '.zip')
+#     shutil.copyfile(os.path.join(models_dir, target_file), zip_file_path)
+#     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+#         zip_ref.extractall(models_dir)
