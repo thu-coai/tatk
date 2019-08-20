e145c8f95cde3cd38c03bfd187050c717ae44cf0
aaa123git <wdz15@tsinghua.org.cn>
2019-08-20 21:33:51 +0800

    finish tests/nlg

--- tests/nlg/template/camrest/test_template_camrest.py
+++ tests/nlg/template/camrest/test_template_camrest.py
@@ -1,7 +1,12 @@
-from tests.nlg.test_nlg import BaseTestNLGCamrest
+from tests.nlg.test_nlg import BaseTestTemplateNLG, BaseTestCamrest
 from tatk.nlg.template.camrest.nlg import TemplateNLG
 
 
-class TestNLGCamrest(BaseTestNLGCamrest):
+class TestTemplateCamrest(BaseTestTemplateNLG, BaseTestCamrest):
+    @classmethod
+    def setup_class(cls):
+        BaseTestTemplateNLG.setup_class()
+        BaseTestCamrest.setup_class()
+
     def test_nlg(self):
         self._test_nlg(TemplateNLG)
