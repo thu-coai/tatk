6165dc286c62a3a6ba04cfa7ad06334bdd547cd0
aaa123git <43716234+aaa123git@users.noreply.github.com>
2019-08-21 15:51:15 +0800

    Tests nlg (#118)
    * refactor: rename a package name in tests/nlg/camrest/test_nlg_camrest.py and tests/nlg/multiwoz/test_nlg_multiwoz.py
    
    * refactor: mv tatk/tests/nlg/multiwoz to tatk/tests/nlg/template_nlg/multiwoz; mv tatk/tests/nlg/camrest to tatk/tests/nlg/template_nlg/camrest; rename two files
    
    * finish tests/nlg

--- tests/nlg/camrest/test_nlg_camrest.py
+++
@@ -1,7 +0,0 @@
-from tests.nlg.test_nlg import BaseTestNLGCamrest
-from tatk.nlg.template_nlg.camrest.nlg import TemplateNLG
-
-
-class TestNLGCamrest(BaseTestNLGCamrest):
-    def test_nlg(self):
-        self._test_nlg(TemplateNLG)
