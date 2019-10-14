cba448546b201f0f572184cf4adb76d3607f842e
zqwerty <zhuq96@hotmail.com>
2019-10-14 19:14:38 +0800

    fix env

--- tatk/dialog_agent/env.py
+++ tatk/dialog_agent/env.py
@@ -16,11 +16,12 @@ class Environment():
     def reset(self):
         self.usr.init_session()
         self.sys_dst.init_session()
+        return self.sys_dst.state
         
     def step(self, action):
         model_response = self.sys_nlg.generate(action) if self.sys_nlg else action
         observation = self.usr.response(model_response)
-        dialog_act = self.nlu_model.predict(observation) if self.nlu_model else observation
+        dialog_act = self.sys_nlu.predict(observation) if self.sys_nlu else observation
         state = self.sys_dst.update(dialog_act)
         
         reward = self.usr.get_reward()
