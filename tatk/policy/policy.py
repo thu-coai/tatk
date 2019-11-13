3ad287a48bb5cc93d5e3e5531ff549f38bb31396
zqwerty <zhuq96@hotmail.com>
2019-11-13 19:21:00 +0800

    modify module interface

--- tatk/policy/policy.py
+++ tatk/policy/policy.py
@@ -1,11 +1,10 @@
 """Policy Interface"""
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 
 
-class Policy(metaclass=ABCMeta):
+class Policy(ABC):
     """Base class for policy model."""
 
-    @abstractmethod
     def predict(self, state):
         """Predict the next agent action given dialog state.
         
@@ -14,12 +13,43 @@ class Policy(metaclass=ABCMeta):
                 when the DST and Policy module are separated, the type of state is tuple.
                 else when they are aggregated together, the type of state is dict (dialog act).
         Returns:
-            action (dict):
+            action (list of tuples):
+                The next dialog action.
+        """
+        return self.predict_batch([state])[0]
+
+    @abstractmethod
+    def predict_batch(self, batch_state):
+        """Predict actions given a batch of dialog states.
+
+        Args:
+            batch_state (list of tuple or dict):
+                when the DST and Policy module are separated, the type of state is tuple.
+                else when they are aggregated together, the type of state is dict (dialog act).
+        Returns:
+            batch_action (list of list of tuples):
                 The next dialog action.
         """
         pass
 
     @abstractmethod
+    def train(self, *args, **kwargs):
+        """Model training entry point"""
+        pass
+
+    @abstractmethod
+    def test(self, *args, **kwargs):
+        """Model testing entry point"""
+        pass
+
+    def from_cache(self, *args, **kwargs):
+        """restore internal state for multi-turn dialog"""
+        return None
+
+    def to_cache(self, *args, **kwargs):
+        """save internal state for multi-turn dialog"""
+        return None
+
     def init_session(self):
         """Init the class variables for a new session."""
         pass
