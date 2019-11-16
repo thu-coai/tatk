087cc893215a3b045e38445c2f9346e2ce1e2fca
Ryuichi Takanobu <truthless11@gmail.com>
2019-11-16 11:11:55 +0800

    Policy Dataloader (#160)
    * init vhus
    
    * remove old files
    
    * add vhus vocab
    
    * move usermodule in vhus
    
    * vhus for camrest
    
    * fix informable-keys
    
    * fix vhus user manager
    
    * revise config
    
    * sclstm for multiwoz
    
    * move model dir
    
    * sclstm for camrest
    
    * git ignore
    
    * run sclstm in camrest
    
    * sclstm data for camrest
    
    * sclstm: update camrest model
    
    * sclstm download from tatk
    
    * fix
    
    * pseudo policy training script
    
    * update interface for agenda policy
    
    * update interface for vhus policy
    
    * add env
    
    * ppo
    
    * pg
    
    * config for rl policy
    
    * __init__ for rl policy
    
    * README of training RL policy
    
    * init README of policy module
    
    * update policy README
    
    * load model for mle & vhus
    
    * data split in mle & vhus
    
    * from BERT to SVM in example script
    
    * rename
    
    * update README
    
    * update README
    
    * update README
    
    * test for mle policy
    
    * README for mle policy
    
    * init vhus
    
    * remove old files
    
    * move usermodule in vhus
    
    * vhus for camrest
    
    * fix vhus user manager
    
    * update README
    
    * update README
    
    * update README
    
    * test for mle policy
    
    * README for mle policy
    
    * update README
    
    * update README
    
    * update metrics
    
    * update README
    
    * fix conflict
    
    * fix vectorize
    
    * fix vectorize
    
    * fix MLE policy
    
    * is_train for RL policy
    
    * update README
    
    * rm conflict file
    
    * fix VHUS on camrest
    
    * update result
    
    * update camrest data
    
    * fix sclstm on camrest
    
    * performance by table
    
    * add info about models
    
    * update SCLSTM README
    
    * add evaluator into sess
    
    * update README
    
    * init policy dataloader
    
    * framework: dialog act level policy
    
    * add user dialog policy dataloader & abstract mle policy
    
    * simplify vhus policy

---
+++ tatk/policy/mle/loader.py
@@ -0,0 +1,52 @@
+import os
+import pickle
+import torch
+import torch.utils.data as data
+from tatk.util.multiwoz.state import default_state
+from tatk.policy.vector.dataset import ActDataset
+from tatk.util.dataloader.module_dataloader import ActPolicyDataloader
+
+class ActMLEPolicyDataLoader():
+    
+    def __init__(self):
+        self.vector = None
+        
+    def _build_data(self, root_dir, processed_dir):        
+        self.data = {}
+        for part in ['train', 'val', 'test']:
+            self.data[part] = []
+            raw_data = ActPolicyDataloader(data_key=part, role='sys')
+            
+            for turn in raw_data:
+                state = default_state()
+                state['user_action'] = turn['context_dialog_act'][-1]
+                state['system_action'] = turn['context_dialog_act'][-2]
+                state['terminal'] = turn['terminal']
+                action = turn['dialog_act']
+                self.data[part].append([self.vector.state_vectorize(state),
+                         self.vector.action_vectorize(action)])
+        
+        os.makedirs(processed_dir)
+        for part in ['train', 'val', 'test']:
+            with open(os.path.join(processed_dir, '{}.pkl'.format(part)), 'wb') as f:
+                pickle.dump(self.data[part], f)
+
+    def _load_data(self, processed_dir):
+        self.data = {}
+        for part in ['train', 'val', 'test']:
+            with open(os.path.join(processed_dir, '{}.pkl'.format(part)), 'rb') as f:
+                self.data[part] = pickle.load(f)
+                
+    def create_dataset(self, part, batchsz):
+        print('Start creating {} dataset'.format(part))
+        s = []
+        a = []
+        for item in self.data[part]:
+            s.append(torch.Tensor(item[0]))
+            a.append(torch.Tensor(item[1]))
+        s = torch.stack(s)
+        a = torch.stack(a)
+        dataset = ActDataset(s, a)
+        dataloader = data.DataLoader(dataset, batchsz, True)
+        print('Finish creating {} dataset'.format(part))
+        return dataloader
--- tatk/policy/vhus/camrest/vhus.py
+++ tatk/policy/vhus/camrest/vhus.py
@@ -2,20 +2,17 @@
 import os
 import json
 import torch
-import zipfile
-from tatk.util.file_util import cached_path
-from tatk.policy.policy import Policy
-from tatk.policy.vhus.util import padding
 from tatk.task.camrest.goal_generator import GoalGenerator
 from tatk.policy.vhus.camrest.usermanager import UserDataManager
 from tatk.policy.vhus.usermodule import VHUS
+from tatk.policy.vhus.vhus import UserPolicyVHUSAbstract
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 DEFAULT_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models")
 DEFAULT_ARCHIVE_FILE = os.path.join(DEFAULT_DIRECTORY, "vhus_simulator_camrest.zip")
 
-class UserPolicyVHUS(Policy):
+class UserPolicyVHUS(UserPolicyVHUSAbstract):
 
     def __init__(self,
                  archive_file=DEFAULT_ARCHIVE_FILE,
@@ -29,55 +26,4 @@ class UserPolicyVHUS(Policy):
         self.manager = manager
         self.user.eval()
 
-        if not os.path.isfile(archive_file):
-            if not model_file:
-                raise Exception("No model for VHUS Policy is specified!")
-            archive_file = cached_path(model_file)
-        model_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'save')
-        if not os.path.exists(model_dir):
-            os.mkdir(model_dir)
-        if not os.path.exists(os.path.join(model_dir, 'best_simulator.mdl')):
-            archive = zipfile.ZipFile(archive_file, 'r')
-            archive.extractall(model_dir)
-        self.load(config['load'])
-
-    def init_session(self):
-        self.time_step = -1
-        self.topic = 'NONE'
-        self.goal = self.goal_gen.get_user_goal()
-        self.goal_input = torch.LongTensor(self.manager.get_goal_id(self.manager.usrgoal2seq(self.goal)))
-        self.goal_len_input = torch.LongTensor([len(self.goal_input)]).squeeze()
-        self.sys_da_id_stack = []  # to save sys da history
-
-    def predict(self, state):
-        """Predict an user act based on state and preorder system action.
-
-        Args:
-            state (tuple):
-                Dialog state.
-        Returns:
-            usr_action (tuple):
-                User act.
-            session_over (boolean):
-                True to terminate session, otherwise session continues.
-        """
-        sys_action = state
-
-        sys_seq_turn = self.manager.sysda2seq(self.manager.ref_data2stand(sys_action), self.goal)
-        self.sys_da_id_stack += self.manager.get_sysda_id([sys_seq_turn])
-        sys_seq_len = torch.LongTensor([max(len(sen), 1) for sen in self.sys_da_id_stack])
-        max_sen_len = sys_seq_len.max().item()
-        sys_seq = torch.LongTensor(padding(self.sys_da_id_stack, max_sen_len))
-        usr_a, terminal = self.user.select_action(self.goal_input, self.goal_len_input, sys_seq, sys_seq_len)
-        usr_action = self.manager.usrseq2da(self.manager.id2sentence(usr_a), self.goal)
-
-        return usr_action, terminal
-
-    def load(self, filename):
-        user_mdl = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename + '_simulator.mdl')
-        if os.path.exists(user_mdl):
-            self.user.load_state_dict(torch.load(user_mdl))
-            print('<<user simulator>> loaded checkpoint from file: {}'.format(user_mdl))
-
-    def get_goal(self):
-        return self.goal
+        self.load(archive_file, model_file, config['load'])
