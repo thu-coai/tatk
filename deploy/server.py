163f5630852726bb436a914d05d8eed212c5666b
lixiang <lixiang890217@163.com>
2019-11-20 23:49:24 +0800

    add edit_last function

--- deploy/server.py
+++ deploy/server.py
@@ -70,72 +70,80 @@ class ServerCtrl(object):
                 self.modules[module].sub_used_num(sess_info[module])
         return {'del_tokens': del_tokens}
 
-    def on_response(self, token, input_module, data):
+    def on_response(self, token, input_module, data, modified_output={}):
         if not self.sessions.has_token(token):
             raise DeployError('No such token:\'%s\'' % token)
-        sess_info = self.sessions.get_session(token)
-        isfirst = not sess_info['cache']
+        session_infos = self.sessions.get_session(token)
+
+        ret, session_infos = self.turn(session_infos, input_module, data, modified_output)
+
+        self.sessions.set_session(token, session_infos)
+        return ret
+
+    def on_edit_last(self, token, modified_output):
+        if not self.sessions.has_token(token):
+            raise DeployError('No such token:\'%s\'' % token)
+        session_infos = self.sessions.get_session(token)
+
+        if not session_infos['cache']:
+            raise DeployError('This is the first turn in this session.')
+
+        last_cache = session_infos['cache'][-1]
+        session_infos['cache'] = session_infos['cache'][:-1]
+        for (key, value) in last_cache['modified_output'].items():
+            modified_output.setdefault(key, value)
+
+        ret, session_infos = self.turn(session_infos, last_cache['input_module'], last_cache['data'], modified_output)
+
+        self.sessions.set_session(token, session_infos)
+        return ret
+
+    def turn(self, session_infos, input_module, data, modified_output):
+        modules_list = ['nlu', 'dst', 'policy', 'nlg']
+
+        # params
+        cur_cache = {name: None for name in modules_list}
         history = []
-        if isfirst:
-            cache_nlu, cache_dst, cache_plc, cache_nlg = None, None, None, None
-        else:
-            last_cache = sess_info['cache'][-1]
-            cache_nlu = last_cache.get('nlu', None)
-            cache_dst = last_cache.get('dst', None)
-            cache_plc = last_cache.get('policy', None)
-            cache_nlg = last_cache.get('nlg', None)
-            for cache in sess_info['cache']:
+        if session_infos['cache']:
+            cur_cache = {name: session_infos['cache'][-1].get(name, None) for name in modules_list}
+            for cache in session_infos['cache']:
                 history.append(['user', cache.get('usr', '')])
                 history.append(['system', cache.get('sys', '')])
 
-        ret_nlu, ret_dst, ret_plc, ret_nlg = None, None, None, None
-        new_cache_nlu, new_cache_dst, new_cache_plc, new_cache_nlg = None, None, None, None
+        # process
+        new_cache = {name: None for name in modules_list}
+        model_ret = {name: None for name in modules_list}
+        temp_data = None
+        for mod in modules_list:
+            if input_module == mod:
+                temp_data = data
 
-        # NLU
-        if input_module == 'nlu':
-            in_nlu = data
-            if sess_info['nlu'] is not None:
-                (ret_nlu, new_cache_nlu) = self.modules['nlu'].run(sess_info['nlu'], cache_nlu, isfirst, [in_nlu, history])
-                out_nlu = ret_nlu
-            else:
-                out_nlu = in_nlu
+            if temp_data is not None and session_infos[mod] is not None:
+                (model_ret[mod], new_cache[mod]) = self.modules[mod].run(session_infos[mod], cur_cache[mod], not session_infos['cache'],
+                                                                         [temp_data, history] if mod == 'nlu' else [temp_data])
+                if mod in modified_output.keys():
+                    model_ret[mod] = modified_output[mod]
 
-        # DST
-        if input_module in ['nlu', 'dst']:
-            in_dst = out_nlu if input_module == 'nlu' else data
-            if sess_info['dst'] is not None:
-                (ret_dst, new_cache_dst) = self.modules['dst'].run(sess_info['dst'], cache_dst, isfirst, [in_dst])
-                out_dst = ret_dst
-            else:
-                out_dst = in_dst
+                temp_data = model_ret[mod]
+            elif mod == 'policy':
+                temp_data = None
 
-        # POLICY
-        if input_module in ['nlu', 'dst', 'policy']:
-            in_plc = out_dst if input_module in ['nlu', 'dst'] else data
-            if sess_info['policy'] is not None:
-                (ret_plc, new_cache_plc) = self.modules['policy'].run(sess_info['policy'], cache_plc, isfirst, [in_plc])
-                out_plc = ret_plc
-            else:
-                out_plc = None
 
-        # NLG
-        in_nlg = out_plc if input_module in ['nlu', 'dst', 'policy'] else data
-        if sess_info['nlg'] is not None and in_nlg is not None:
-            (ret_nlg, new_cache_nlg) = self.modules['nlg'].run(sess_info['nlg'], cache_nlg, isfirst, [in_nlg])
 
         # save cache
-        new_cache = {
-            'nlu': new_cache_nlu, 'dst': new_cache_dst, 'policy': new_cache_plc, 'nlg': new_cache_nlg,
-            'usr': data if isinstance(data, str) and input_module == 'nlu' else '',
-            'sys': ret_nlg if isinstance(ret_nlg, str) else ''
-        }
-        sess_info['cache'].append(copy.deepcopy(new_cache))
-        self.sessions.set_session(token, sess_info)
+        new_cache['usr'] = data if isinstance(data, str) and input_module == 'nlu' else ''
+        new_cache['sys'] = model_ret['nlg'] if isinstance(model_ret['nlg'], str) else ''
+        new_cache['input_module'] = input_module
+        new_cache['data'] = data
+        new_cache['modified_output'] = modified_output
+        session_infos['cache'].append(copy.deepcopy(new_cache))
 
-        history.append(['user', new_cache.get('usr', '')])
-        history.append(['system', new_cache.get('sys', '')])
+        # update history
+        history.append(['user', new_cache['usr']])
+        history.append(['system', new_cache['sys']])
+        model_ret['history'] = history
 
-        return {'nlu': ret_nlu, 'dst': ret_dst, 'policy': ret_plc, 'nlg': ret_nlg, 'history': history}
+        return model_ret, session_infos
 
     def on_rollback(self, token, back_turns=1):
         if not self.sessions.has_token(token):
